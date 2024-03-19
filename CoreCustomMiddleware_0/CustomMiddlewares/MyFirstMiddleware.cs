f36c8d77bff3abd81ab4184ff9298bc6b6a84946
Ekrem Güngör <ekrmdsgnr@gmail.com>
2024-03-19 22:04:19 +0300

    Add project files.

---
+++ CoreCustomMiddleware_0/CustomMiddlewares/MyFirstMiddleware.cs
@@ -0,0 +1,21 @@
+﻿namespace CoreCustomMiddleware_0.CustomMiddlewares
+{
+    public class MyFirstMiddleware
+    {
+        RequestDelegate _requestDelegate;
+
+        public MyFirstMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+        //Metot isminin Invoke olmasına ve HttpContext tipinde parametre almasına dikkat ediniz...
+        public async Task Invoke(HttpContext context)
+        {
+            int a = context.Response.StatusCode;
+            if (context.Request.Path == "/Deneme")
+                await context.Response.WriteAsync("Deneme path'i tespit edildi");
+            else
+                await _requestDelegate.Invoke(context);
+        }
+    }
+}
---
+++ CoreCustomMiddleware_0/CustomMiddlewares/MySecondMiddleware.cs
@@ -0,0 +1,22 @@
+﻿namespace CoreCustomMiddleware_0.CustomMiddlewares
+{
+    public class MySecondMiddleware
+    {
+        RequestDelegate _requestDelegate;
+
+        public MySecondMiddleware(RequestDelegate requestDelegate)
+        {
+            _requestDelegate = requestDelegate;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            await _requestDelegate.Invoke(context);
+
+            if (context.Response.StatusCode == StatusCodes.Status200OK)
+                await context.Response.WriteAsync("Basarili");
+            else
+                await context.Response.WriteAsync("Bulunamadi");
+        }
+    }
+}
